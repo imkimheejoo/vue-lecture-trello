9505329b162d5187e816271b5d630e9de72d3ab5
imkimheejoo <jujudamdadi@naver.com>
2020-01-18 21:22:47 +0900

    refactor: router 관련 로직 분리

--- src/main.js
+++ src/main.js
@@ -1,21 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-//이걸 추가해야 온전히 Vue-Router를 사용할 수 있음
-Vue.use(VueRouter)
-
-const Login = {template: '<div>Login Page</div>'}
-const NotFound = {template: '<div>Page Not Found</div>'}
-
-const router = new VueRouter({
-  mode: 'history', // # 없애기 위해
-  routes: [
-    {path: '/', component: App},
-    {path: '/login', component: Login},
-    {path: '*', component: NotFound}
-  ]
-})
+import router from './router' //index는 생략가능 .js도 생략가능
 
 new Vue({
   el: '#app',
