09e903b8ab736afdfc49ac4bff7d1da9bb31891b
imkimheejoo <jujudamdadi@naver.com>
2020-01-18 20:05:41 +0900

    init : init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
