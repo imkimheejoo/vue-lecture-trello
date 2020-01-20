5248fa21038cd3f5cf8466969bf8d26f28648e79
imkimheejoo <jujudamdadi@naver.com>
2020-01-20 16:23:21 +0900

    feat: 데이터 불러올 때 라우팅 구현

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     Board
-    <div>bid: {{bid}}</div>
-    <hr />
-<!--    Board component에 Card component를 어디에 출력할지 정해야함 => 중첩 라우팅-->
-    <router-link :to="`/b/${bid}/c/1`">Card1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card2</router-link>
-    <router-view></router-view>
+    <!--    데이토 로딩이 완료 됐을 떄 출력-->
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid: {{bid}}</div>
+      <hr/>
+      <!--    Board component에 Card component를 어디에 출력할지 정해야함 => 중첩 라우팅-->
+      <router-link :to="`/b/${bid}/c/1`">Card1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card2</router-link>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -15,11 +19,22 @@
         name: "Board",
         data() {
             return {
-                bid:0
+                bid: 0,
+                loading: false
             }
         },
         created() {
-            this.bid = this.$route.params.bid
+            this.fetchData()
+        },
+        methods: {
+            //백엔드 api를 호출하고 데이터를 요청하는 함수 -> 데이터를 받아서 데이터 객체에 추가함
+            fetchData() {
+                this.loading = true
+                setTimeout(() => {
+                    this.bid = this.$route.params.bid  //이 명령어 실핼하소 0.5초 지연
+                    this.loading = false
+                }, 500)
+            }
         }
     }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     Card
-    <div>cid: {{cid}}</div>
+    <div v-if="loading">card loading...</div>
+    <div v-else>
+      <div>cid: {{cid}}</div>
+    </div>
   </div>
 </template>
 
@@ -9,16 +12,25 @@
     export default {
         name: "Card",
         data() {
-            return{
-                cid: 0
+            return {
+                cid: 0,
+                loading: false
             }
         },
-        created() {
-            this.cid = this.$route.params.cid
-        },
         watch: {
-            '$route'() {
-                this.cid = this.$route.params.cid //route객체를 감시
+            '$route': {
+                //fetchData를 실행시켜라
+                handler: 'fetchData',
+                immediate: true //즉시 실행해라 (created의 역할도 해줌)
+            }
+        },
+        methods: {
+            fetchData() {
+                this.loading = true
+                setTimeout(() =>{
+                    this.cid = this.$route.params.cid
+                    this.loading = false
+                }, 500)
             }
         }
     }
