a1fed103f76dfa41dee7be196ffd537e725a51cb
imkimheejoo <jujudamdadi@naver.com>
2020-01-21 04:11:50 +0900

    feat: 보드목록 조회 template 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>Home
-    <div>
-      Board List
-      <div v-if="loading">로딩중</div>
-      <div v-else>
-        <div v-for="b in boards" :key="b.id">
-          {{b}}
-        </div>
+  <div>
+    <div class="home-title">Personal Boards</div>
+    <div class="board-list" ref="boardList">
+      <div class="board-item" v-for="b in boards" :key="b.id"
+           :data-bgcolor="b.bgColor" ref="boardItem">
+        <router-link :to="`/b/${b.id}`">
+          <div class="board-item-title">{{b.title}}</div>
+        </router-link>
+      </div>
+      <div class="board-item board-item-new">
+        <a class="new-board-btn" href="" @click.prevent="addBoard">
+          Create new board...
+        </a>
       </div>
     </div>
   </div>
@@ -21,26 +26,86 @@
             return {
                 loading: false,
                 boards: [],
+                error: ''
             }
         },
         created() {
             this.fetchData()
+        },
+        updated() {
+            this.$refs.boardItem.forEach(el => {
+                el.style.backgroundColor = el.dataset.bgcolor
+            })
         },
         methods: {
             fetchData() {
                 this.loading = true
                 board.fetch()
                     .then(data => {
-                        this.boards = data
+                        this.boards = data.list
                     })
                     .finally(_ => {
                         this.loading = false
                     })
+            },
+            addBoard() {
+                console.log('addBoard')
             }
         }
     }
 </script>
 
 <style scoped>
+  .home-title {
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+  }
 
+  .board-list {
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .board-item {
+    width: 23%;
+    height: 100px;
+    margin: 0 2% 20px 0;
+    border-radius: 3px;
+  }
+
+  .board-item-new {
+    background-color: #ddd;
+  }
+
+  .board-item a {
+    text-decoration: none;
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
+  .board-item a:hover,
+  .board-item a:focus {
+    background-color: rgba(0, 0, 0, .1);
+    color: #666;
+  }
+
+  .board-item-title {
+    color: #fff;
+    font-size: 18px;
+    font-weight: 700;
+    padding: 10px;
+  }
+
+  .board-item a.new-board-btn {
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    height: 100px;
+    width: inherit;
+    color: #888;
+    font-weight: 700;
+  }
 </style>
