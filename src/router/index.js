ea5872883ae4ae34920ef95e1f0e7d99b5ecb2c1
imkimheejoo <jujudamdadi@naver.com>
2020-01-20 22:11:25 +0900

    feat: 네비게이션 가드 로직 구현

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,21 @@ import Card from "../components/Card";
 //이걸 추가해야 온전히 Vue-Router를 사용할 수 있음
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token') //없으면 undefined 값이 저장
+  //로그인이 완료되면 현재페이지로 돌아오게 하려면 rPath=${encodeURIComponent(to.path)}
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history', // # 없애기 위해
   routes: [
-    {path: '/', component: Home},
+    {path: '/', component: Home, beforeEnter: requireAuth},
     {path: '/login', component: Login},
     {
-      path: '/b/:bid', component: Board, children: [
-        {path: 'c/:cid', component: Card} //주소 맨 앞에 /없음
+      path: '/b/:bid', component: Board, beforeEnter: requireAuth, children: [
+        {path: 'c/:cid', component: Card, beforeEnter: requireAuth} //주소 맨 앞에 /없음
       ]
     },
     {path: '*', component: NotFound}
