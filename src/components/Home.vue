392e9c7778d01c7bf61a430d5f2627726ca27865
imkimheejoo <jujudamdadi@naver.com>
2020-01-20 17:18:06 +0900

    feat: 순수 브라우저 api만 이용해서 http call을 날리는 로직 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,8 @@
   <div>Home
     <div>
       Board List
+      <div v-if="loading">로딩중</div>
+      <div v-else>Api result: {{apiRes}}</div>
       <ul>
         <li>
           <router-link to="/b/1">Board1</router-link>
@@ -16,7 +18,37 @@
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        data() {
+            return {
+                loading: false,
+                apiRes: ''
+            }
+        },
+        created() {
+            this.fetchData()
+        },
+        methods: {
+            fetchData() {
+                this.loading = true
+
+                const req = new XMLHttpRequest() //요청객체 생성
+
+                req.open('GET', 'http://localhost:3000/health')
+                req.send()
+
+                req.addEventListener('load', () => {
+                    this.loading = false
+                    this.apiRes = {
+                        status: req.status,
+                        statusText: req.statusText,
+                        response: JSON.parse(req.response) //순수 문자열로 오기 때문에 json으로 파싱 해줘야 함
+                    }
+                })
+
+
+            }
+        }
     }
 </script>
 
